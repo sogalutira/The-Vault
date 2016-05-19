a092db9fd8a9b7d6acfc4974878a98303b4ed50a
sgalutira <sheena.galutira@gmail.com>
2016-05-18 20:15:07 -1000

    null if no key used

--- vault.js
+++ vault.js
@@ -1,4 +1,19 @@
 'use strict';
 module.exports = function() {
-  
+    var theVault = {};
+  function setValue(key, value) {
+      theVault[key] = value;
+    }
+     function getValue(key) {
+      if (theVault[key]) {
+        return theVault;
+      }else{
+      return null;
+    }
+  }
+    return {
+      "setValue": setValue,
+      "getValue": getValue
+    };
+
 };
